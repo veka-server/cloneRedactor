23a6f5241f6fccfe3a80d7c2b0de617edd728733
Veka <veka61@laposte.net>
2013-03-11 14:21:49 +0100

    inital commit

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+ * Fichier de configuration de cloneRedactor
+ */
+
+// Chemin absolu vers le dossier d'upload
+$dossier_upload = '';
+
+// URL vers le dossier d'upload
+$url_upload = 'http://...';
+
+
+/*
+
+N'oubliez pas de definir la page d'upload dans le fichier cloneRedactor.js
+
+*/
