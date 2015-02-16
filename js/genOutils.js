9fec3a26a3cdb01809681e3dec227ecb3c3f82ee
Veka <veka61@laposte.net>
2015-02-16 12:42:49 +0100

    Update genOutils.js

--- js/genOutils.js
+++ js/genOutils.js
@@ -1,18 +1,18 @@
 // generation des outils
 (function(){
-	// requis
-	var cloneRedactor = window.cloneRedactor = window.cloneRedactor || {};
-	
-	var genOutils = function(outils){
+    // requis
+    var cloneRedactor = window.cloneRedactor = window.cloneRedactor || {};
+    
+    var genOutils = function(outils){
         if(outils == undefined)
         {
           var outils = ['html', '|', 'format', '|', 'undo', 'redo', '|', 'bold', 'italic', 'underline', 'rayer', '|', 'link', 'unlink', 'picture', '|', 'color', '|', 'orderedlist', 'unorderedlist','|', 'alignleft', 'aligncenter', 'alignright', 'justify'];
         }
 
-	    // contenu des bulles d'aide au survole des boutons
-        outils_title = {'html':'Switch','bold':'Gras','italic':'Italique','underline':'Sousligner','rayer':'Rayer','link':'Creer un lien','unlink':'Supprimer un lien','picture':'Creer une image','color':'Changer la couleur du texte','orderedlist':'Liste ordonné','unorderedlist':'Liste non ordonné','alignleft':'Aligner a gauche','aligncenter':'Aligner au centre','alignright':'Aligner a droite','justify':'Justifier','format':'Format du texte', 'undo':'Annuler la dernière action','redo':'Retablir la dernière action', 'format':'Paragraphe'};
+        // contenu des bulles d'aide au survole des boutons
+        outils_title = {'html':'Switch','bold':'Gras','italic':'Italique','underline':'Sousligner','rayer':'Rayer','link':'Creer un lien','unlink':'Supprimer un lien','picture':'Creer une image','color':'Changer la couleur du texte','orderedlist':'Liste ordonn\351','unorderedlist':'Liste non ordonn\351','alignleft':'Aligner a gauche','aligncenter':'Aligner au centre','alignright':'Aligner a droite','justify':'Justifier','format':'Format du texte', 'undo':'Annuler la derni\350re action','redo':'Retablir la derni\350re action', 'format':'Paragraphe'};
 
-	    // genere le code HTML integrant la liste des outils
+        // genere le code HTML integrant la liste des outils
         var list_outils = [];
         for(var i = 0; i < outils.length; i++)
         {
@@ -34,19 +34,19 @@
         }
 
         return list_outils;
-	}
+    }
 
-	cloneRedactor.genConfig = function(element, outils){
+    cloneRedactor.genConfig = function(element, outils){
 
-		// genere l'id du cloneRedactor
-	    cloneRedactor.getUniqueId();
+        // genere l'id du cloneRedactor
+        cloneRedactor.getUniqueId();
 
-		var retour = {
-		  UniqueId 	: 	cloneRedactor.lastId,
-		  contenu	: 	element.textContent,
-		  outils 	: 	genOutils(outils)
-		};
+        var retour = {
+          UniqueId  :   cloneRedactor.lastId,
+          contenu   :   element.textContent,
+          outils    :   genOutils(outils)
+        };
 
-		return retour;
-	}
+        return retour;
+    }
 })();
